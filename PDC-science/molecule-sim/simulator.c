be9f5334fae08bfebcbc3513df1c2619e93259ec
jake-87 <jhhenderson87@gmail.com>
2021-04-09 12:36:13 +1000

    more science stuff aaaaaa

--- PDC-science/molecule-sim/simulator.c
+++ PDC-science/molecule-sim/simulator.c
@@ -6,6 +6,7 @@
 #define MAXELEC 5
 #define SRTELEC 2
 #define MOLNUMB 10
+double kevinsmath();
 int main()
 {
 	double molecules[MOLNUMB];
@@ -15,15 +16,53 @@ int main()
 		molecules[i] = SRTELEC;
 		printf("Initing molecule %d\n",i);
 	}
-	printf("pselect 5 \"over\" 2 = %f\n",psel(5,2));
-	printf("select 5 \"over\" 2 = %f\n",sel(5,2));
+	kevinsmath(4.0,1.0,5.0);
 }
 double kevinsmath(double er1,double er2,double etot)
 {
 	double eoxy1 = er1 - 2.0;
 	double eoxy2 = er2 - 2.0;
+	double d,c;
 	if (eoxy1 - eoxy2 == 0.0)
 	{
-		double d = 0.0;
+		d = 1.0;
 	}
+	else
+	{
+		d = 0.0;
+	}
+	if (fabs(eoxy1 * eoxy2) == 0)
+	{
+		c = 1.0;
+	}
+	else
+	{
+		c = 0.0;
+	}
+	printf("c and d are %f and %f\n",c,d);
+	double S = (((eoxy1 - eoxy2)/((fabs(eoxy1 - eoxy2) + c)) + d));
+	printf("S is %f\n",S);
+	if (er1 == 0.0)
+	{
+		printf("0\n");
+		return 0;
+	}
+	if (er1 == 1.0)
+	{
+		double result = ((1.0/etot) * (2.0/(psel(etot,1.0))) * (1.0/3.0) * (S));
+		printf("result for er1 = 1 is %f\n",result);
+		return result;
+	}
+	if (er1 == etot)
+	{
+		printf("%f\n",S/3.0);
+		return (S / 3.0);
+	}
+	else 
+	{
+		double result = ((er1/etot) * ((2 * (sel(er1,1) * psel(etot - 2, er1 - 1)) + (sel(er1,1) * psel(etot - 3,er1 - 2)) / psel(etot,er1))) * (1.0/3.0) * (S));
+		printf("result for er1 = %f is %f\n",er1,result);
+		return result;
+	}
+	return 0;
 }
--- PDC-science/molecule-sim/simulator.h
+++ PDC-science/molecule-sim/simulator.h
@@ -12,15 +12,13 @@ double fact(double number, int ident)
 	{
 		return 1.0;
 	}
-	printf("factorialing %f\n",number);
 	return(number * fact(number - 1,ident));
 }
 double psel(double n, double k)
 {
-	printf("in select function");
 	return((fact(n,-1)) / (fact(n - k,-1)));
 }
 double sel(double n, double k)
 {
-	return((fact(n,-2)) / ((fact(k,-2)) - (fact(n - k,-2))));
+	return((fact(n,-2)) / ((fact(k,-2)) * (fact(n - k,-2))));
 }
