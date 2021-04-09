a6da25aa9d9308320a7a5178c5a574e7623d5a5f
jake-87 <jhhenderson87@gmail.com>
2021-04-09 11:40:07 +1000

    more sci stuff

--- PDC-science/molecule-sim/simulator.c
+++ PDC-science/molecule-sim/simulator.c
@@ -3,7 +3,22 @@
 #include <math.h>
 #include <unistd.h>
 #include "simulator.h"
+#define MAXELEC 5
+#define SRTELEC 2
+#define MOLNUMB 10
 int main()
 {
-	
+	double molecules[MOLNUMB];
+	double etot = MAXELEC;
+	for (int i = 0; i <= MOLNUMB; i++)
+	{
+		molecules[i] = SRTELEC;
+		printf("Initing molecule %d\n",i);
+	}
+}
+double kevinsmath(double er1,double er2,double etot)
+{
+	double eneg1 = er1 - 2.0;
+	double eneg2 = er2 - 2.0;
+	S = ( ( () / () ) )
 }
--- config-dots/nvim/ftplugin/c.vim
+++ config-dots/nvim/ftplugin/c.vim
@@ -1,3 +1,1 @@
 let b:ale_fixers = ['remove_trailing_lines', 'trim_whitespace', 'astyle']
-inoremap { {<CR>}<Esc>ko
-inoremap {(} {()h}
