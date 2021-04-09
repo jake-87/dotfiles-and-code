6ab163942bf3db0fa63f3bb512f5ed5054d7b1d9
jake-87 <jhhenderson87@gmail.com>
2021-04-09 16:52:02 +1000

    comments babeee

--- PDC-science/molecule-sim/simulator.c
+++ PDC-science/molecule-sim/simulator.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 #include <math.h>
 #include <unistd.h>
-#include "simulator.h"
+#include "simulator.h" //include a crap ton of stuff, including our custom functions fact(), psel() and sel() (sidenote: Seriously math.h, for a maths lib, its kinda stoopid that you dont include a factorial function)
 #define MAXELEC 5
 #define SRTELEC 2
-#define MOLNUMB 10
-long double kevinsmath();
+#define MOLNUMB 10 // some constants 
+long double kevinsmath(); // sidenote 2: bloody everything has to be long doubles because of pricision, meaning its kinda bad not gonna lie, buts thats how the cookie crumbles
 int main()
 {
-	long double molecules[MOLNUMB];
+	long double molecules[MOLNUMB]; // init main vars, if i need malloc here someone open a PR
 	long double etot = MAXELEC;
 	for (int i = 0; i <= MOLNUMB; i++)
 	{
-		molecules[i] = SRTELEC;
+		molecules[i] = SRTELEC; // init out mol array
 	}
 	for (long double i = 0.f; i <=  ( long double ) MAXELEC; i++)
 	{
 		for (long double j = 0.f; j <= ( long double ) MAXELEC; j++)
 		{
-			printf("i is %Lf, j is %Lf ",i,j);
-			kevinsmath(i,j, etot);
+			printf("i is %Lf, j is %Lf ",i,j); // apply the equation to every possible combination of molecules
+			kevinsmath(i,j, etot); // ^^^^^
 		}
 	}
 }
 long double kevinsmath(long double er1,long double er2,long double etot)
 {
-	long double eoxy1 = er1 - 2.0;
+	long double eoxy1 = er1 - 2.0; // oxy states
 	long double eoxy2 = er2 - 2.0;
 	long double d,c;
 	if (eoxy1 - eoxy2 == 0.0)
@@ -44,30 +44,30 @@ long double kevinsmath(long double er1,long double er2,long double etot)
 	else
 	{
 		c = 0.0;
-	}
-	long double S = ( ( ( eoxy1 - eoxy2 ) / ( fabsl( eoxy1 * eoxy2 ) + c ) ) + d );
+	} //define d and c according to the above rules dependant on the oxy states
+	long double S = ( ( ( eoxy1 - eoxy2 ) / ( fabsl( eoxy1 * eoxy2 ) + c ) ) + d ); // define S according to the oxy states and stuff
 	printf("S is %Lf ",S);
 	if (er1 == 0.0)
 	{
 		printf("result is 0\n");
-		return 0;
+		return 0; // no math case 1
 	}
 	if (er1 == 1.0)
 	{
 		long double result = ((1.0/etot) * (2.0/(psel(etot,1,1))) * (1.0/3.0) * (S));
 		printf("result is %Lf\n",result);
-		return result;
+		return result; // math case 1
 	}
 	if (er1 == etot)
 	{
 		printf("result is %d\n",1);
-		return (1);
+		return (1); // this should really be case 2, but lmao i cant be bothered to fix it cause this case will never arrise anyway (if im the one using the program), so anyone else using this: dont input 1,x,1 into the program or you will get the wrong result ok?
 	}
 	else 
 	{
 		long double result = ((er1/etot) * ((((2 * (sel(er1,1,2) * psel(etot - 2, er1 - 1,3)) + (sel(er1,1,4) * psel(etot - 3,er1 - 2,5)))) / psel(etot,er1,6))) * (1.0/3.0) * (S));
-		printf("result is %Lf\n",result);
+		printf("result is %Lf\n",result); // biggie math time and math case 2, this uses a heap of functions from the header file, so go look at that for moar info
 		return result;
 	}
-	return 0;
+	return 0; // this does not need to be here but the compiler complains otherwise and its annoying
 }
--- PDC-science/molecule-sim/simulator.h
+++ PDC-science/molecule-sim/simulator.h
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-long double fact(long double number, int ident)
+long double fact(long double number, int ident) // this computes factorials of long doubles, dispite the fact that it cant actually understand anything other than a whole number, it gets given long doubles and it has to return long doubles so it has to suffer
 {
 	if (number < 0.0)
 	{
 		printf("Oops! You tried to factorial a negative number! Ident is %d\n",ident);
 		printf("Number was %Lf\n",number);
-		exit(-1);
+		exit(-1); // negative case
 	}
 	if (number == 0.0)
 	{
-		return 1.0;
+		return 1.0; // normal useage case
 	}
-	double a = (number * fact(number - 1,ident));
+	double a = (number * fact(number - 1,ident)); //recursion which im sure is very bad practice but it works so who cares? not me
 	return a;
 }
 long double psel(long double n,long double k,int ident)
 {
-	return((fact(n,ident)) / (fact(n - k,ident)));
+	return((fact(n,ident)) / (fact(n - k,ident))); // combanatorics, i dont understand it any more than you do, so read here if you want to: https://en.wikipedia.org/wiki/Combinatorics , to the best of my ability it just does a bunch of probablility math using factorials and stuff
 }
 long double sel(long double n,long double k,int ident)
 {
-	return((fact(n,ident)) / ((fact(k,ident)) * (fact(n - k,ident))));
+	return((fact(n,ident)) / ((fact(k,ident)) * (fact(n - k,ident)))); // see above, but this one is slightly different
 }
