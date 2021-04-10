77f753af0ba824d1e15750cc537524e30458d064
jake-87 <jhhenderson87@gmail.com>
2021-04-10 21:38:09 +1000

    done with science debuggin, finally

--- PDC-science/molecule-sim/simulator.c
+++ PDC-science/molecule-sim/simulator.c
@@ -2,34 +2,78 @@
 #include <stdlib.h>
 #include <math.h>
 #include <unistd.h>
-#include "simulator.h" //include a crap ton of stuff, including our custom functions fact(), psel() and sel() (sidenote: Seriously math.h, for a maths lib, its kinda stoopid that you dont include a factorial function)
+#include "simulator.h" //include a bunch of stuff, including our custom functions fact(), psel() and sel() (sidenote: Seriously math.h, for a maths lib, its kinda stoopid that you dont include a factorial function)
 #define MAXELEC 5
 #define SRTELEC 2
+#define LOOPS 10
 #define MOLNUMB 10 // some constants 
 long double kevinsmath(); // sidenote 2: bloody everything has to be long doubles because of pricision, meaning its kinda bad not gonna lie, buts thats how the cookie crumbles
 int main()
 {
+	unsigned long seed = mix(clock(),time(NULL),getpid());
+	srand(seed);
 	long double molecules[MOLNUMB]; // init main vars, if i need malloc here someone open a PR
 	long double etot = MAXELEC;
-	for (int i = 0; i <= MOLNUMB; i++)
+	for (int i = 0; i < MOLNUMB; i++)
 	{
 		molecules[i] = SRTELEC; // init out mol array
 	}
-	for (long double i = 0.f; i <=  ( long double ) MAXELEC; i++)
+	long double psrnd = doublerand();
+	long double ngrnd = negdoublerand();
+	printf("pos rand is %Lf, neg rand is %Lf\n",psrnd,ngrnd);
+	printf("running 'da math™'\n");
+	long double result = 0;
+	long double randomnumber;
+	long long endelecs = 0;
+	for (int loopnum = 0; loopnum < LOOPS; loopnum++)
 	{
-		for (long double j = 0.f; j <= ( long double ) MAXELEC; j++)
+		for (int loop2 = 0; loop2 < MOLNUMB; loop2++)
 		{
-			printf("i is %Lf, j is %Lf ",i,j); // apply the equation to every possible combination of molecules
-			kevinsmath(i,j, etot); // ^^^^^
+			result = kevinsmath(molecules[loop2],molecules[loop2 + 1],etot);
+			if (result > 0)
+			{
+				randomnumber = doublerand();
+				if (randomnumber < result)
+				{
+					molecules[loop2]--;
+					molecules[loop2 + 1]++;
+				}
+			}
+			else 
+			{
+				randomnumber = negdoublerand();
+				if (randomnumber > result)
+				{
+					molecules[loop2]++;
+					molecules[loop2 + 2]--;
+				}
+			}
 		}
+		if (molecules[MOLNUMB - 1] > 0)
+		{
+			endelecs = endelecs + molecules[MOLNUMB - 1];
+			molecules[MOLNUMB - 1] = 0;
+		}
+		molecules[0] = etot;
+		
 	}
+	printf("Total number of iterations of the maths equation: %lld, end result of electrons: %lld\n", (long long) (LOOPS * MOLNUMB),endelecs);
+	return 0;
 }
 long double kevinsmath(long double er1,long double er2,long double etot)
 {
 	long double eoxy1 = er1 - 2.0; // oxy states
 	long double eoxy2 = er2 - 2.0;
-	long double d,c;
-	if (eoxy1 - eoxy2 == 0.0)
+	long double d,c,chk1 = 0,chk2 = 0;
+	if (comparedouble(etot,er1))
+	{
+		chk1 = 1.0;
+	}
+	if (comparedouble(er2,etot))
+	{
+		chk2 = 1.0;
+	}
+	if (comparedouble(eoxy1 - eoxy2,0.0))
 	{
 		d = 1.0;
 	}
@@ -37,7 +81,7 @@ long double kevinsmath(long double er1,long double er2,long double etot)
 	{
 		d = 0.0;
 	}
-	if (fabsl(eoxy1 * eoxy2) == 0)
+	if (comparedouble(fabsl(eoxy1 * eoxy2),0.0))
 	{
 		c = 1.0;
 	}
@@ -46,28 +90,32 @@ long double kevinsmath(long double er1,long double er2,long double etot)
 		c = 0.0;
 	} //define d and c according to the above rules dependant on the oxy states
 	long double S = ( ( ( eoxy1 - eoxy2 ) / ( fabsl( eoxy1 * eoxy2 ) + c ) ) + d ); // define S according to the oxy states and stuff
-	printf("S is %Lf ",S);
-	if (er1 == 0.0)
+	if (comparedouble(er1,0.0))
 	{
-		printf("result is 0\n");
 		return 0; // no math case 1
 	}
-	if (er1 == 1.0)
+	if (comparedouble(er1,1.0))
 	{
 		long double result = ((1.0/etot) * (2.0/(psel(etot,1,1))) * (1.0/3.0) * (S));
-		printf("result is %Lf\n",result);
 		return result; // math case 1
 	}
-	if (er1 == etot)
+	if (er1 >= etot)
 	{
-		printf("result is %d\n",1);
-		return (1); // this should really be case 2, but lmao i cant be bothered to fix it cause this case will never arrise anyway (if im the one using the program), so anyone else using this: dont input 1,x,1 into the program or you will get the wrong result ok?
+		if (er2 >= etot)
+		{
+			return 0;
+		}
+		return 1; // this should really be case 2, but lmao i cant be bothered to fix it cause this case will never arrise anyway (if im the one using the program), so anyone else using this: dont input 1,x,1 into the program or you will get the wrong result ok?
 	}
 	else 
 	{
-		long double result = ((er1/etot) * ((((2 * (sel(er1,1,2) * psel(etot - 2, er1 - 1,3)) + (sel(er1,1,4) * psel(etot - 3,er1 - 2,5)))) / psel(etot,er1,6))) * (1.0/3.0) * (S));
-		printf("result is %Lf\n",result); // biggie math time and math case 2, this uses a heap of functions from the header file, so go look at that for moar info
-		return result;
+		long double part1 = ((er1/etot) * (1.0/3.0) * (S));
+		long double part2 = (2 * (sel(er1,1,20) * psel(etot - 2, er1 - 1, 21)));
+		long double part3 = (sel(er1, 2, 21) * psel(etot - 3, er1 - 2, 22));
+		long double part4 = (psel(etot,er1,23));
+		long double betterresult = (part1 * ((part2 + part3) / part4));
+		return betterresult;
 	}
-	return 0; // this does not need to be here but the compiler complains otherwise and its annoying
+	printf("there should be no way something gets here, and it is an error if something does: we are gonna exit now\n");
+	exit(1);
 }
--- PDC-science/molecule-sim/simulator.h
+++ PDC-science/molecule-sim/simulator.h
@@ -1,20 +1,47 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-long double fact(long double number, int ident) // this computes factorials of long doubles, dispite the fact that it cant actually understand anything other than a whole number, it gets given long doubles and it has to return long doubles so it has to suffer
+#include <time.h>
+#include <unistd.h>
+long double comparedouble(long double a, long double b)
 {
-	if (number < 0.0)
+	if (fabsl(a - b) < 0.00000001)
+	{
+		return 1.f;
+	}
+	else
+	{
+		return 0.f;
+	}
+}
+unsigned long mix(unsigned long a, unsigned long b, unsigned long c)
+{
+    a=a-b;  a=a-c;  a=a^(c >> 13);
+    b=b-c;  b=b-a;  b=b^(a << 8);
+    c=c-a;  c=c-b;  c=c^(b >> 13);
+    a=a-b;  a=a-c;  a=a^(c >> 12);
+    b=b-c;  b=b-a;  b=b^(a << 16);
+    c=c-a;  c=c-b;  c=c^(b >> 5);
+    a=a-b;  a=a-c;  a=a^(c >> 3);
+    b=b-c;  b=b-a;  b=b^(a << 10);
+    c=c-a;  c=c-b;  c=c^(b >> 15);
+    return c;
+} // evil bitwise hack
+long double fact(long double dubnumber, int ident) // this computes factorials of long doubles, dispite the fact that it cant actually understand anything other than a whole number, it gets given long doubles and it has to return long doubles so it has to suffer
+{
+	int number = dubnumber;
+	if (number < 0)
 	{
 		printf("Oops! You tried to factorial a negative number! Ident is %d\n",ident);
-		printf("Number was %Lf\n",number);
+		printf("Number was %d\n",number);
 		exit(-1); // negative case
 	}
-	if (number == 0.0)
+	if (number == 0)
 	{
 		return 1.0; // normal useage case
 	}
 	double a = (number * fact(number - 1,ident)); //recursion which im sure is very bad practice but it works so who cares? not me
-	return a;
+	return (long double) a;
 }
 long double psel(long double n,long double k,int ident)
 {
@@ -23,4 +50,14 @@ long double psel(long double n,long double k,int ident)
 long double sel(long double n,long double k,int ident)
 {
 	return((fact(n,ident)) / ((fact(k,ident)) * (fact(n - k,ident)))); // see above, but this one is slightly different
+}
+long double doublerand()
+{
+	long double random = ((long double)rand()/RAND_MAX);
+	return random;
+}
+long double negdoublerand()
+{
+	long double random = ((long double)rand()/RAND_MAX);
+	return random * -1.0;
 }
